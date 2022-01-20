59b5858f538a2541997e17ca2c4a11542cc7c706
Alaet <72195619+Alaet@users.noreply.github.com>
2022-01-20 15:35:38 +0100

    update js
    add function to check arrow/modal

--- Js/api.js
+++ Js/api.js
@@ -5,6 +5,16 @@ const BEST_ADVENTURE = "http://localhost:8000/api/v1/titles/?genre=Adventure&sor
 const BEST_HORROR = "http://localhost:8000/api/v1/titles/?genre=Horror&sort_by=-imdb_score&page_size=7&page=1";
 const BEST_COMEDY = "http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score&page_size=7&page=1";
 
+let positions = {
+  all_categories: 1,
+  action: 1,
+  adventure: 1,
+  horror: 1,
+  comedy: 1
+}
+
+const nb_visible_elt = 4;
+
 const sendRequest = async function(url) {
 // Return the data from a request to an url 
 try {
@@ -28,17 +38,28 @@ const loadData = async function(urllist) {
     return data
 }
 
-let displayTopMovieData = function(topRanked) {
+let displayTopMovieData = async function(topRanked) {
     // Put the top movie data in the 'movie' blocks
     const block = document.querySelector(".top-movie__information");
-    block.innerHTML = (
-        "<strong>" + topRanked.title + "</strong>"
-        + "<br><strong>Genre(s) :</strong> " + topRanked.genres
-        + "<br><strong>Année :</strong> " + topRanked.year
-        + "<br><strong>Note :</strong> " + topRanked.imdb_score
-        + "<br><strong>Acteurs :</strong> " + topRanked.actors
-        + "<br><strong>Producteur :</strong> " + topRanked.directors
+    try {
+      const response = await fetch(topRanked.url);
+      const jsonResponse = await response.json();
+
+      console.log(jsonResponse)
+      block.innerHTML = (
+        "<strong style='text-shadow:black 2px 2px; color:wheat';>" + topRanked.title + "</strong>"  
+        + "<br><strong style='text-shadow:black 2px 5px; color:Cornsilk'>Genre(s) :</strong> " + topRanked.genres
+        + "<br><strong style='text-shadow:black 2px 5px; color:Cornsilk'>Année :</strong> " + topRanked.year
+        + "<br><strong style='text-shadow:black 2px 5px; color:Cornsilk'>Note :</strong> " + topRanked.imdb_score
+        + "<br><strong style='text-shadow:black 2px 5px; color:Cornsilk'>Acteurs :</strong> " + topRanked.actors
+        + "<br><strong style='text-shadow:black 2px 5px; color:Cornsilk'>Producteur :</strong> " + topRanked.directors
+        + "<br><strong style='text-shadow:black 2px 5px; color:Cornsilk'>Durée :</strong> " + jsonResponse.duration + "min"
     );
+      }
+    catch (e) {
+      logError(e);
+      }
+    
   }
 
 
@@ -61,7 +82,7 @@ let displayTopMovieData = function(topRanked) {
     const h2s = categoryBlock.querySelectorAll(".movie h2");
     for (let i = 0; i < articles.length; i++) {
         articles[i].style.backgroundImage = "url('" + categoryData[i].image_url + "')";
-        h2s[i].innerHTML = categoryData[i].title;
+        //h2s[i].innerHTML = categoryData[i].title;
     }
   }
   let displayMovieData = function(categoryBlock, categoryData) {
@@ -80,20 +101,44 @@ let displayTopMovieData = function(topRanked) {
     }
   }
 
-let setPage = async function() {
-  n_data = await loadData([TOP_RANKED, BEST_ALL_CATEGORIES, BEST_ACTION, BEST_ADVENTURE, BEST_HORROR, BEST_COMEDY]);
-  topMovieRanked = n_data[0]
-  const categoryBlocks = document.querySelectorAll(".category");
-    //Categories
-    for (let i = 0; i < categoryBlocks.length; i++) {
-        displayMoviePreview(categoryBlocks[i], n_data[(i + 1)]); // "+ 1" due to the fact that the first element of the array isn't for a category
-        displayMoviePosters(categoryBlocks[i], n_data[(i + 1)]);
-        displayMovieData(categoryBlocks[i], n_data[(i + 1)]);
-    }
-  const topMovieData = document.querySelector('#top-movie__poster');
-  topMovieData.style.backgroundImage = "url('" + topMovieRanked[0].image_url + "')";
-  console.log(n_data[0][0].image_url)
-  displayTopMovieData(topMovieRanked[0]);
+let checkArrows = async function(category, nb_total_elt) {
+  // Change the side arrows to active or to disable according to the position
+  if (positions[category] == 1) {
+      document.querySelector("#" + category +  " .left").classList.add("side-arrow--disable");   
+  } else if (positions[category] > (nb_total_elt - nb_visible_elt)) {
+      document.querySelector("#" + category +  " .right").classList.add("side-arrow--disable");
+  } else {
+      document.querySelector("#" + category +  " .left").classList.remove("side-arrow--disable");
+      document.querySelector("#" + category +  " .right").classList.remove("side-arrow--disable");
+  }
+}
+let setVisibleMovies = async function(parent, position) {
+  // Display or hide the movies in the categories depending of the position of the category
+  const nb_total_elt = 7;
+  for(let elt_nb = 1; elt_nb <= nb_total_elt; elt_nb++) {
+      if ((elt_nb >= position) && (elt_nb < (nb_visible_elt + position))) {
+          document.querySelector(parent + " .category__best-movies .movie:nth-child(" + elt_nb + ")").classList.remove("movie--hidden");
+      }else {
+          document.querySelector(parent + " .category__best-movies .movie:nth-child(" + elt_nb + ")").classList.add("movie--hidden");
+      }
+  }
+}
+
+let changeCategoryPosition = async function(category, direction) {
+  // Change the position inside the category
+  const nb_total_elt = document.querySelectorAll("#" + category + " .movie").length;
+  if (direction == "right") {
+      if (positions[category] <= (nb_total_elt - nb_visible_elt)) {
+      positions[category] ++;
+      setVisibleMovies("#" + category, positions[category]);
+      }
+  } else {
+      if (positions[category] > 1) {
+      positions[category] --;
+      setVisibleMovies("#" + category, positions[category]);
+      }
+  }
+  checkArrows(category, nb_total_elt);
 }
 
 let openModal = async function(movie) {
@@ -102,6 +147,21 @@ let openModal = async function(movie) {
 let closeModal = async function(movie) {
 document.querySelector("#" + movie + " .modal").style.visibility = "hidden";
 } 
+
+let setPage = async function() {
+  data = await loadData([TOP_RANKED, BEST_ALL_CATEGORIES, BEST_ACTION, BEST_ADVENTURE, BEST_HORROR, BEST_COMEDY]);
+  topMovieRanked = data[0]
+  const categoryBlocks = document.querySelectorAll(".category");
+    //Categories
+    for (let i = 0; i < categoryBlocks.length; i++) {
+        displayMoviePreview(categoryBlocks[i], data[(i + 1)]); // "+ 1" due to the fact that the first element of the array isn't for a category
+        displayMoviePosters(categoryBlocks[i], data[(i + 1)]);
+        displayMovieData(categoryBlocks[i], data[(i + 1)]);
+    }
+  const topMovieData = document.querySelector('#top-movie__poster');
+  topMovieData.style.backgroundImage = "url('" + topMovieRanked[0].image_url + "')";
+  displayTopMovieData(topMovieRanked[0]);
+}
 
 setPage()
 
